22e9ab214127145ff2f5dd6c6a89a4f9413bb1f1
Evgeny Leshchenko <evgeny.leshchenko@gmail.com>
2015-06-12 12:47:45 +0000

    remove comments block

--- resources/views/home/articles/show.blade.php
+++ resources/views/home/articles/show.blade.php
@@ -15,29 +15,13 @@
 		about {{ $article->created_at->diffForHumans() }}.</div>
 		{!! $article->body_html !!}
 		@unless(is_null($article->original) || empty($article->original))
-			<p>参考来源:<br>{{ $article->original }}</p>
+			<p>original:<br>{{ $article->original }}</p>
 		@endunless
-		<p>本文链接:<br>{{ setting('site_url').$article->slug }}</p>
+		<p>link:<br>{{ setting('site_url').$article->slug }}</p>
 	</div><!-- /.blog-post -->
 	<hr>
 
-	<!-- 多说评论框 start -->
 	<div class="ds-thread" data-thread-key="{{ $article->slug }}" data-title="{{ $article->title }}" data-url="{{ setting('site_url').$article->slug }}"></div>
-	<!-- 多说评论框 end -->
-	<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
-	<script type="text/javascript">
-	var duoshuoQuery = {short_name:"90door"};
-		(function() {
-			var ds = document.createElement('script');
-			ds.type = 'text/javascript';ds.async = true;
-			ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.unstable.js';
-			ds.charset = 'UTF-8';
-			(document.getElementsByTagName('head')[0]
-			 || document.getElementsByTagName('body')[0]).appendChild(ds);
-		})();
-		</script>
-	<!-- 多说公共JS代码 end -->
-
 
 </div><!-- /.blog-main -->
 @stop
