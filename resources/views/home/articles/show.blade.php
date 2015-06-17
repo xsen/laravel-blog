7efc76ebb6b1cb6bec50cfeabb0bf29dc062804f
Evgeny Leshchenko <evgeny.leshchenko@gmail.com>
2015-06-17 14:43:29 +0000

    add VK.com comments

--- resources/views/home/articles/show.blade.php
+++ resources/views/home/articles/show.blade.php
@@ -17,6 +17,26 @@
                     {!! $article->body_html !!}
                 </div>
             </div>
+
+            @if (setting('comment_on'))
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <!-- Put this script tag to the <head> of your page -->
+                        <script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
+
+                        <script type="text/javascript">
+                            VK.init({apiId: 4961783, onlyWidgets: true});
+                        </script>
+
+                        <!-- Put this div tag to the place, where the Comments block will be -->
+                        <div id="vk_comments"></div>
+                        <script type="text/javascript">
+                            VK.Widgets.Comments("vk_comments", {limit: 10, width: "680", attach: "*"});
+                        </script>
+                    </div>
+                </div>
+            @endif
         </div>
     </article>
+
 @stop
